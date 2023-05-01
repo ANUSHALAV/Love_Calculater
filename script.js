9e81173365680730af8fb4e1ddb227a43f3aa386
anushalav_bhatt <bhattanushalav@gmail.com>
2023-05-01 15:33:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let clickbtn=()=>{
+    if(validdate()){
+        let result=Math.floor(Math.random()*100);
+        document.getElementById('result').value=result+"%";
+        document.getElementById('yname').value="";
+        document.getElementById('lname').value="";
+
+    }
+}
+
+let validdate=()=>{
+    let yourname=document.getElementById('yname').value;
+    let lovename=document.getElementById('lname').value;
+    if(yourname.length==""){
+        alert("please enter your name");
+        return false;
+    }
+    else if(lovename.length==""){
+        alert("please enter your lover name");
+        return false;
+    }
+    else if(yourname.length<=2){
+        alert("Minimum 3 chracter allowed recheack your name");
+        return false;
+    }
+    else if(lovename.length<=2){
+        alert("Minimum 3 chracter allowed recheack your lover name");
+        return false;
+    }
+    return true;
+}
